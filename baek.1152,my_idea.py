bb4a1fb75e6e276e91e95b8497a2c47a9ddd3c2f
chanu2 <mdsoo55828@gmail.com>
2022-02-08 20:43:41 +0900

    word count_my-idea

---
+++ baek.1152,my_idea.py
@@ -0,0 +1,27 @@
+n=input()
+sum=1
+if n[0]==' 'and n[len(n)-1]!=' ':
+    for i in range(1,len(n)):
+        if n[i]==' ':
+            sum+=1
+    print(sum)       
+          
+elif n[0]!=' 'and n[len(n)-1] ==' ':
+    for j in range(len(n)-1):
+        if n[j]==' ':
+            sum+=1
+    print(sum)        
+# elif n[0]==' 'and n[len(n)-1]==' ':
+#     for k in range(1,len(n)-1):
+#         if n[k]==' ':
+#             sum+=1
+elif n[0]!=' 'and n[len(n)-1]!=' ':
+    for l in range(len(n)):
+        if n[l]==' ':
+            sum+=1
+    print(sum)       
+
+elif n[0]==' ':
+    print(0) 
+
+
