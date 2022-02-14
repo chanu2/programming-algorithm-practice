d281c561e3b179e59968982d12b6e49d6f049c2e
chanu2 <mdsoo55828@gmail.com>
2022-02-14 13:03:03 +0900

    minimum_time taken

--- baek_greedy_11399.py
+++ baek_greedy_11399.py
@@ -1,8 +1,8 @@
 n=int(input())
 sum=0
 personal_time=list(map(int, input().split()))  
-personal_time.sort()   # 오름차순으로 정렬한다.
+personal_time.sort()   # 오름차순으로 정렬한다.  //(최소 소요시간을 구하기 위해)
 for i in range(n):
-    for j in range(i+1):   # 이중 반복문을 통해서 각각의 소요 시간을 구하는 코드
-        sum+=personal_time[j]   # 각각의 소요 시간을 모두 합하는 코드
+    for j in range(i+1):   # 이중 반복문을 통해서 개인의 소요시간을 구하는 코드
+        sum+=personal_time[j]   # 개인의 소요시간을 모두 합하는 코드
 print(sum)
