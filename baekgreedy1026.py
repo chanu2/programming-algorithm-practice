27af3838c9b19d71552e008c10258e700dfa9a1a
chanu2 <mdsoo55828@gmail.com>
2022-02-28 22:40:09 +0900

    pop().ex

--- baekgreedy1026.py
+++ baekgreedy1026.py
@@ -1,11 +1,11 @@
 sum=0
 n=int(input())
-A=list(map(int,input().split()))
-B=list(map(int,input().split()))
+A=list(map(int,input().split()))   # 리스트로 여러개의 정수를 받는다
+B=list(map(int,input().split())) 
 for i in range(n):
-    sum+=min(A)*max(B)
-    A.pop(A.index(min(A)))
-    B.pop(B.index(max(B)))
+    sum+=min(A)*max(B)         #각 리스트의 최소값과 최대값을 구한다 
+    A.pop(A.index(min(A)))     # pop()를 사용하여 최소값이 있는 인덱스를 제거한다
+    B.pop(B.index(max(B)))     # pop()를 사용하여 최대값이 있는 인덱스를 제거한다
 print(sum)
 
     
