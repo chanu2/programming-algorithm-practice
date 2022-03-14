6f1ffc50b8d76e81851b81fe4fe89f0d9dc04b89
chanu2 <mdsoo55828@gmail.com>
2022-03-15 01:16:28 +0900

    heap queue

--- baek_greedy_1715.py
+++ baek_greedy_1715.py
@@ -1,26 +1,16 @@
-# import sys
-# n=int(sys.stdin.readline())
-# sum=0
-# number=[int(input()) for _ in range(n)]
-# number.sort()
-# sum=0
-# for i in range(1,len(number)):
-#     sum+=number[0]
-#     for j in range(1,i+1):
-#         sum+=number[j]
-# print(sum)
 import sys
+import heapq     #우선순위 큐 -> 완전 이진트리
 n=int(sys.stdin.readline())
-number=[int(input()) for _ in range(n)]
-number.sort()
+number=[int(sys.stdin.readline()) for _ in range(n)]
+heapq.heapify(number)  #오름차순 정렬 (logn)
 sum=0
-if len(number)==1:
-    print(number[0])
+result=0
 
-else:
-    j=len(number)-1
-    sum+=number[0]*j
-    for i in range(1,len(number)):
-        sum+=number[i]*j
-        j-=1
-    print(sum)  
+    
+for _ in range(len(number)-1):
+    a=heapq.heappop(number)   # 가장 최소값을 pop한다
+    b=heapq.heappop(number) 
+    sum=a+b
+    result+=sum
+    heapq.heappush(number,sum)  # 자동으로 순서에 맞추어 push된다
+print(result)
