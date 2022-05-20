29612882cbfc6f0fa2ba4b7e9aecce3505d9992d
chanu2 <mdsoo55828@gmail.com>
2022-05-20 23:17:15 +0900

    divide

---
+++ Baekjoon/Data_Structure/baek_datastructue_19583.py
@@ -0,0 +1,13 @@
+import sys
+dic={}
+while True:
+    
+    try:
+        
+        time,user=map(str,sys.stdin.readline().split())
+        dic[user]=time
+
+    except 
+        break       
+
+print(dic)    
