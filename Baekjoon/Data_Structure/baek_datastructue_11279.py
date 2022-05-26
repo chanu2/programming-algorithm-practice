91da05d4d048840355f364fc7f2d70d7e7beb54f
chanu2 <mdsoo55828@gmail.com>
2022-05-26 23:28:55 +0900

    heapq modify

--- Baekjoon/Data_Structure/baek_datastructue_11279.py
+++ Baekjoon/Data_Structure/baek_datastructue_11279.py
@@ -5,12 +5,12 @@ n=int(sys.stdin.readline().rstrip())
 for i in range(n):
     data=int(sys.stdin.readline())
     if data!=0:
-        heapq.heappush(heap,(-data,data))
+        heapq.heappush(heap,(-data,data))  # 집합으로 heap에 넣을 수 있다.
     else:
-        if heap:
+        if not heap:
             print(0)
         else:
-            a=heapq.heappop(heap)
+            a=heapq.heappop(heap)   # 꺼낼 때 -data기준으로 뽑을 수 있다
             print(a[1])    
 
 
